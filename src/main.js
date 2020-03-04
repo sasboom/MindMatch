d421357c93daa30b208a13386e24d3809ee623ff
SASBoom <balrajshah@live.com>
2020-03-03 23:07:33 -0500

    Add VueApollo

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from "./router";
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { createProvider } from './vue-apollo'
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
@@ -12,5 +13,6 @@ Vue.config.productionTip = false;
 
 new Vue({
   router,
+  apolloProvider: createProvider(),
   render: h => h(App)
 }).$mount("#app");
