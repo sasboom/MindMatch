f1e085c569e67ba69ca05b44c6594b969923517b
SASBoom <balrajshah@live.com>
2020-03-07 11:10:05 -0500

    Add Matchfinder view

--- src/views/MatchFinder.vue
+++ src/views/MatchFinder.vue
@@ -1,44 +1,59 @@
 <template>
-  <ApolloQuery :query="require('../graphql/GetRandomUsers.gql')">
-    <template v-slot="{ result: { loading, error, data } }">
-      <!-- Loading -->
-      <div v-if="loading">Loading...</div>
+  <div>
+    <Navbar />
+    <ApolloQuery :query="require('../graphql/GetRandomUsers.gql')">
+      <template v-slot="{ result: { loading, error, data } }">
+        <!-- Loading -->
+        <div v-if="loading">Loading...</div>
 
-      <!-- Error -->
-      <div v-else-if="error">An error occurred</div>
+        <!-- Error -->
+        <div v-else-if="error">An error occurred</div>
 
-      <!-- Result -->
-      <b-container v-else-if="data" class="card-set">
-        <b-card-group deck>
-          <div v-for="user in data.randomUsers.payload" :key="user.id">
-            <b-card
-              :title="user.username"
-              img-src="https://picsum.photos/600/300/?image=25"
-              img-alt="Image"
-              img-top
-              style="max-width: 20rem;"
-              class="mb-2"
-            >
-              <b-card-text>User introduction</b-card-text>
-              <b-card-text>{{user.firstName}} {{user.lastName}}</b-card-text>
-              <b-button-group vertical>
-                <b-button href="#" variant="outline-success" class="actionBtns">Send Request</b-button>
-                <b-button href="#" variant="outline-danger" class="actionBtns">Decline</b-button>
-              </b-button-group>
-            </b-card>
-          </div>
-        </b-card-group>
-      </b-container>
+        <!-- Result -->
+        <b-spinner v-if="isLoading" label="Spinning"></b-spinner>
+        <b-container v-else-if="data && !isLoading" class="card-set">
+          <b-card-group columns>
+            <div v-for="user in data.randomUsers.payload" :key="user.id">
+              <b-card :title="user.username" style="max-width: 20rem;" class="mb-4">
+                <b-card-text>User introduction</b-card-text>
+                <b-card-text>{{user.firstName}} {{user.lastName}}</b-card-text>
+                <b-button-group vertical>
+                  <b-button href="#" variant="outline-success" class="actionBtns">Send Request</b-button>
+                  <b-button href="#" variant="outline-danger" class="actionBtns">Decline</b-button>
+                </b-button-group>
+                <template v-slot:footer>
+                  <small class="text-muted">Last updated 3 mins ago</small>
+                </template>
+              </b-card>
+            </div>
+          </b-card-group>
+        </b-container>
 
-      <!-- No result -->
-      <div v-else>No result :(</div>
-    </template>
-  </ApolloQuery>
+        <!-- No result -->
+        <div v-else>No result :(</div>
+      </template>
+    </ApolloQuery>
+  </div>
 </template>
 
 <script>
+import Navbar from "../components/Navbar";
 export default {
-  name: "MatchFinder"
+  name: "MatchFinder",
+  data() {
+    return {
+      isLoading: false
+    };
+  },
+  created: function() {
+    this.isLoading = true;
+  },
+  mounted: function() {
+    this.isLoading = false;
+  },
+  components: {
+    Navbar
+  }
 };
 </script>
 
@@ -47,6 +62,6 @@ export default {
   margin-bottom: 1vh;
 }
 .card-set {
-  margin-top: 2vh;
+  margin-top: 4vh;
 }
 </style>
