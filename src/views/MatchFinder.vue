fb58b221392d0dfa332d952fc1a8b51e9761994d
SASBoom <balrajshah@live.com>
2020-03-08 23:50:25 -0400

    Add match request mutation

--- src/views/MatchFinder.vue
+++ src/views/MatchFinder.vue
@@ -1,58 +1,106 @@
 <template>
   <div>
     <Navbar />
-    <ApolloQuery :query="require('../graphql/GetRandomUsers.gql')">
-      <template v-slot="{ result: { loading, error, data } }">
-        <!-- Loading -->
-        <div v-if="loading">Loading...</div>
-
-        <!-- Error -->
-        <div v-else-if="error">An error occurred</div>
-
-        <!-- Result -->
-        <b-spinner v-if="isLoading" label="Spinning"></b-spinner>
-        <b-container v-else-if="data && !isLoading" class="card-set">
-          <b-card-group columns>
-            <div v-for="user in data.randomUsers.payload" :key="user.id">
-              <b-card :title="user.username" style="max-width: 20rem;" class="mb-4">
-                <b-card-text>User introduction</b-card-text>
-                <b-card-text>{{user.firstName}} {{user.lastName}}</b-card-text>
-                <b-button-group vertical>
-                  <b-button href="#" variant="outline-success" class="actionBtns">Send Request</b-button>
-                  <b-button href="#" variant="outline-danger" class="actionBtns">Decline</b-button>
-                </b-button-group>
-                <template v-slot:footer>
-                  <small class="text-muted">Last updated 3 mins ago</small>
-                </template>
-              </b-card>
-            </div>
-          </b-card-group>
-        </b-container>
-
-        <!-- No result -->
-        <div v-else>No result :(</div>
-      </template>
-    </ApolloQuery>
+    <MessageAlert
+      :show="alertProps.show"
+      :variant="alertProps.variant"
+      :message="alertProps.message"
+      :dismissedFn="resetAlertProps"
+    />
+    <b-spinner v-if="isLoading" label="Spinning"></b-spinner>
+    <b-container v-else-if="randomUsers && !isLoading" class="card-set">
+      <b-card-group columns>
+        <div v-for="user in randomUsers" :key="user.id">
+          <b-card :title="user.username" style="max-width: 20rem;" class="mb-4">
+            <b-card-text>User introduction</b-card-text>
+            <b-card-text>{{user.firstName}} {{user.lastName}}</b-card-text>
+            <b-button-group vertical>
+              <b-button
+                @click="sendRequest(user)"
+                variant="outline-success"
+                class="actionBtns"
+              >Send Request</b-button>
+              <b-button href="#" variant="outline-danger" class="actionBtns">Decline</b-button>
+            </b-button-group>
+            <template v-slot:footer>
+              <small class="text-muted">Last updated 3 mins ago</small>
+            </template>
+          </b-card>
+        </div>
+      </b-card-group>
+    </b-container>
+    <!-- No result -->
+    <div v-else>No result :(</div>
   </div>
 </template>
 
 <script>
 import Navbar from "../components/Navbar";
+import MessageAlert from "../components/MessageAlert";
+import MATCH_REQUEST from "../graphql/MatchRequest.gql";
+import RANDOM_USERS_QUERY from "../graphql/GetRandomUsers.gql";
+
 export default {
   name: "MatchFinder",
   data() {
     return {
-      isLoading: false
+      isLoading: false,
+      alertProps: {
+        show: false,
+        variant: "",
+        message: ""
+      },
+      randomUsers: []
     };
   },
   created: function() {
     this.isLoading = true;
+    this.$apollo
+      .query({
+        query: RANDOM_USERS_QUERY
+      })
+      .then(response => {
+        this.randomUsers = response.data.randomUsers.payload;
+      });
   },
   mounted: function() {
     this.isLoading = false;
   },
   components: {
-    Navbar
+    Navbar,
+    MessageAlert
+  },
+  methods: {
+    resetAlertProps() {
+      this.alertProps = {
+        show: false,
+        variant: "",
+        message: ""
+      };
+    },
+    sendRequest(user) {
+      return this.$apollo
+        .mutate({
+          mutation: MATCH_REQUEST,
+          variables: {
+            input: {
+              initiatorUserId: "6e3ab5ee-80aa-44ef-9395-8db9fe761c29",
+              requestedUserId: user.id
+            }
+          },
+          update: () => {
+            this.randomUsers = this.randomUsers.filter(i => i.id !== user.id);
+            this.alertProps = {
+              show: true,
+              variant: "success",
+              message: `A match request has been sent out to ${user.firstName} ${user.lastName} AKA ${user.username}.`
+            };
+          }
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
   }
 };
 </script>
