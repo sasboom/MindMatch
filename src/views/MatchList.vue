bca9393b1171a23b8791fa2d0cbe485882aced3f
sasboom <balrajshah@live.com>
2020-06-21 20:39:12 -0400

    Modify cards for different statuses and add update status for match request

--- src/views/MatchList.vue
+++ src/views/MatchList.vue
@@ -1,36 +1,55 @@
 <template>
   <div>
     <Navbar />
+    <MessageAlert
+      :show="alertProps.show"
+      :variant="alertProps.variant"
+      :message="alertProps.message"
+      :dismissedFn="resetAlertProps"
+    />
     <b-spinner v-if="isLoading" label="Spinning"></b-spinner>
-    <b-container v-else-if="matches && !isLoading" class="card-set">
+    <b-container v-else-if="matches.length > 0 && !isLoading" class="card-set">
       <b-card-group columns>
         <div v-for="match in matches" :key="match.id">
           <b-card :title="match.initiator.username" style="max-width: 20rem;" class="mb-4">
-            <b-card-text>User introduction</b-card-text>
             <b-card-text>{{match.initiator.firstName}} {{match.initiator.lastName}}</b-card-text>
             <b-button-group vertical>
               <b-button
-                @click="sendRequest(user)"
+                @click="sendRequest(match, 'APPROVED')"
                 variant="outline-success"
                 class="actionBtns"
+                v-if="match.status === 'PENDING'"
               >Accept</b-button>
-              <b-button href="#" variant="outline-danger" class="actionBtns">Decline</b-button>
+              <b-button
+                @click="sendRequest(match, 'REJECTED')"
+                variant="outline-danger"
+                class="actionBtns"
+                v-if="match.status === 'PENDING'"
+              >Decline</b-button>
+              <b-button
+                @click="startChat(match)"
+                variant="success"
+                class="actionBtns"
+                v-if="match.status === 'APPROVED'"
+              >Start Chat </b-button>
             </b-button-group>
             <template v-slot:footer>
-              <small class="text-muted">Status: {{match.status}}</small>
+              <small class="text-muted">Status:<b-card-text v-bind:class="{'status-text-approved': match.status === 'APPROVED', 'status-text-rejected': match.status === 'REJECTED'}">{{match.status}}</b-card-text></small>
             </template>
           </b-card>
         </div>
       </b-card-group>
     </b-container>
     <!-- No result -->
-    <div v-else>No result :(</div>
+    <div v-else-if="matches.length <= 0">No result :(</div>
   </div>
 </template>
 
 <script>
 import Navbar from "../components/Navbar";
+import MessageAlert from '../components/MessageAlert';
 import GET_MATCHES from "../graphql/GetMatches.gql";
+import UPDATE_MATCH from "../graphql/MatchUpdate.gql";
 import moment from 'moment';
 
 export default {
@@ -64,6 +83,7 @@ export default {
   },
   components: {
     Navbar,
+    MessageAlert
   },
   methods: {
     resetAlertProps() {
@@ -73,6 +93,30 @@ export default {
         message: ""
       };
     },
+    sendRequest({id}, status) {
+      return this.$apollo
+        .mutate({
+          mutation: UPDATE_MATCH,
+          variables: {
+            input: {
+              id: id,
+              status: status
+            }
+          },
+          update: () => {
+            this.matches.map(i => {
+              if (i.id === id) {
+                i.status = status;
+              }
+            });
+            this.alertProps = {
+              show: true,
+              variant: "success",
+              message: `The match has been approved.`
+            };
+          }
+        })
+    }
   }
 };
 </script>
@@ -84,4 +128,13 @@ export default {
 .card-set {
   margin-top: 4vh;
 }
+.status-text-approved {
+  font-weight: bold;
+  color: green;
+}
+.status-text-rejected {
+  font-weight: bold;
+  color: red;
+}
+
 </style>
